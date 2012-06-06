7f836c66b864e4b51b054f34c0ef7d2cb6e76567
unknown <Jens@Jens-PC.(none)>
2012-06-06 16:54:36 +0200

    Presentation added. PlotData.m modified.

--- exercises/exercise7/plotData.m
+++ exercises/exercise7/plotData.m
@@ -5,18 +5,23 @@ figure();
 N = size(data,1);
 
 for i = 1:N
-   for j = 1:N
-       subplot(N,N,(i-1)*N+j);
-       if(i ~= j)
-          scatter(data(i,:),data(j,:));
-       else
-          title(strcat(['X',num2str(i)]));
-       end
-   end
+	subplot(N,1,i);
+	plot(data(i,:));
+	title(strcat(['S',num2str(i)]));
 end
 
-suptitle(strTitle);
+%for i = 1:N
+%   for j = 1:N
+%       subplot(N,N,(i-1)*N+j);
+%       if(i ~= j)
+%          scatter(data(i,:),data(j,:));
+%       else
+%          title(strcat(['X',num2str(i)]));
+%       end
+%   end
+%end
 
+suptitle(strTitle);
 
 end
 
